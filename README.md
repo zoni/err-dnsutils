0c2063206b5e2aea452c7caaee1ffc23c73b18af
Nick Groenen <zoni@zoni.nl>
2012-10-07 17:31:19 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+err-dnsutils
+============
+
+Run common DNS utils from Err: host, dig, nslookup
+
+### Installation
+
+You can install err-dnsutils directly from err using:
+
+	!repos install err-dnsutils
+
+	Alternatively, you can also clone this repository into your BOT\_EXTRA\_PLUGIN\_DIR and restart your bot to load the new plugin.
+
+### Configuration
+
+This plugin requires no configuration at this time
+
+### Dependencies
+
+No extra python dependencies are required, however you do need to have host, dig and nslookup installed on your system.
+
+Most Linux distributions have these packaged together as dnsutils. On Gentoo the package you need is called net-dns/bind-tools
+
---
+++ dnsutils.py
@@ -0,0 +1,85 @@
+#  err-dnsutils: Run common DNS utils: host, dig, nslookup
+#  Copyright (C) 2012 Nick 'zoni' Groenen <zoni@zoni.nl>
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from errbot.botplugin import BotPlugin
+from errbot import botcmd
+import logging
+from subprocess import Popen, PIPE, STDOUT
+
+logger = logging.getLogger('errbot.botplugin.DnsUtils')
+
+# Version of the message that's triggered after installing the plugin
+# Incrementing this ensures the message is re-triggered, even if it had
+# already been triggered in the past.
+INSTALL_MESSAGE_VERSION = 1
+# Message text to send to bot admins upon installing/updating plugin
+INSTALL_MESSAGE_TEXT = """Thank you for installing err-dnsutils.
+
+Please note that this plugin calls dig/nslookup/host and passes arguments on to them directly without any form of checking.
+Although this should be safe (commands are called directly, not via shell which could be tricked into redirecting output or executing arbitrary commands), this might still give users more direct access than desired. If you're really paranoid (like me!) you may want to uninstall this plugin.
+
+For more information about this plugin, or to reports bugs and feature requests, please visit the GitHub page at https://github.com/zoni/err-dnsutils"""
+
+class DnsUtils(BotPlugin):
+	"""Run common DNS utils: host, dig, nslookup"""
+
+	def activate(self):
+		super(DnsUtils, self).activate()
+		if not 'INSTALL_MESSAGE_VERSION' in self.shelf.keys() or self.shelf['INSTALL_MESSAGE_VERSION'] < INSTALL_MESSAGE_VERSION:
+			self.warn_admins(INSTALL_MESSAGE_TEXT)
+			self.shelf['INSTALL_MESSAGE_VERSION'] = INSTALL_MESSAGE_VERSION
+			self.shelf.sync() # Not strictly needed, but I like being safe
+
+		# A little sanity checking
+		for cmd in (['dig'], ['nslookup', 'localhost'], ['host']):
+			try:
+				Popen(cmd, stdout=PIPE, stderr=STDOUT).communicate()[0]
+			except Exception, e:
+				self.warn_admins("Warning! This plugin uses {0}, but it failed to execute: {1}".format(cmd[0], e))
+
+	def execute(self, cmd, args):
+		"""Execute a command and return it's result
+
+		Args:
+			cmd: A string pointing to the executable to run
+			args: A list of arguments to pass to the executable
+		Returns: String containing output
+		"""
+		try:
+			return Popen([cmd] + args, stdout=PIPE, stderr=STDOUT).communicate()[0]
+		except OSError, e:
+			logger.exception(e)
+			return "Failed to run {0}: {1}".format(cmd, e)
+
+	@botcmd
+	def dig(self, mess, args):
+		return self.execute('dig', args.split())
+
+	@botcmd
+	def nslookup(self, mess, args):
+		args = args.split()
+		if len(args) < 1 or args[0] in ('-', '-interactive'):
+			# Passing no arguments, or with first argument beginning with - or -interactive
+			# to nslookup causes it to hang due to dropping into interactive mode. There's
+			# probably a nicer way to deal with this though
+			return "Your call to nslookup would have caused it to go into interactive mode and hang so I aborted it, sorry"
+		else:
+			return self.execute('nslookup', args)
+
+	@botcmd
+	def host(self, mess, args):
+			return self.execute('host', args.split())
+
